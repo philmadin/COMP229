950f3afcdde2b4b86f77f2286d95419c038919c2
41511905 <philip.madin@students.mq.edu.au>
2016-08-03 17:52:28 +1000

    Sheep and Wolves

--- src/main.java
+++ src/main.java
@@ -3,7 +3,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println("~~~~~");
+		System.out.println("Sheep and Wolves");
+		System.out.println("~~~~~");
 	}
 
 }
